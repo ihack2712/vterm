db06878425e3698ed8ab324e6caae2667cedaa8a
James Bradlee <james@ihacks.dev>
2020-11-16 15:03:33 +0100

    x Make the linter happy.

--- lib/errors.ts
+++ lib/errors.ts
@@ -1,11 +1,11 @@
 export class ScreenBufferError extends Error {
-  public name: string = "ScreenBufferError";
+	public name = "ScreenBufferError";
 }
 
 export class ResizeError extends ScreenBufferError {
-  public name: string = "ScreenBufferError";
+	public name = "ScreenBufferError";
 }
 
 export class RepositionError extends ScreenBufferError {
-  public name: string = "RepositionError";
+	public name = "RepositionError";
 }
