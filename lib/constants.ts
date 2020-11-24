003d9019b22ea17665f912a9cf0e8252aa00bcda
James Bradlee <james@ihacks.dev>
2020-11-24 23:08:22 +0100

    :sparkles: Name changes and commenting.

--- lib/constants.ts
+++ lib/constants.ts
@@ -17,22 +17,24 @@ export const ESC = 0x1B; // \x1b - Escape Character
 export const DEL = 0x3B; // ; - Delimeter
 export const CSO = 0x5B; // [ - Control Sequencse Opening
 export const PM = 0x6D; // m - Graphic Mode
+export const QM = 0x3F; // ?
+export const AH = 0x68; // h
+export const AL = 0x6C; // l
 
 export const CSI = [ESC, CSO];
 export const G_RESET = [...CSI, PM];
 
-export const GM_RESET = 0x00;
 export const GM_BOLD = 0x01;
 export const GM_DIM = 0x02;
 export const GM_ITALIC = 0x03;
 export const GM_UNDERLINE = 0x04;
-export const GM_DBLUNDERLINE_OFF_BOLD = 0x15;
 export const GM_SLOW_BLINK = 0x05;
 export const GM_REVERSE = 0x07;
 export const GM_HIDE = 0x08;
 export const GM_STRIKE = 0x09;
+export const GM_DBL_UNDERLINE = 0x15;
 
-export const GO_BOLD_DIM = 0x16;
+export const GO_INTENSITY = 0x16; // bold & dim
 export const GO_ITALIC = 0x17;
 export const GO_UNDERLINE = 0x18;
 export const GO_BLINK = 0x19;
